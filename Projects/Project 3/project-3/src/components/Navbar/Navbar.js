9ddd8f008e4ebe98446f9499b7da5d12b5c39d84
SubasicSeida <seida.subasic@stu.ibu.edu.ba>
2024-06-04 22:46:40 +0200

    started navigation

---
+++ Projects/Project 3/project-3/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navbar.css'
+import { assets } from "../../assets/assets";
+
+function Navbar(){
+    return (
+        <div className="navbar">
+        </div>
+    )
+}
+
+export default Navbar
